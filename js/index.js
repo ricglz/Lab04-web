0184d3558391db70f75a7efd81e7ea3efd0d7e43
Ricardo González Castillo <rjgcastillo@hotmail.com>
2020-03-11 09:25:36 -0600

    Minor fix in security and displaying the log

--- js/index.js
+++ js/index.js
@@ -1,40 +1,46 @@
-let operation = "";
-const inputNumber = document.getElementById('inputNumber');
-const operatorIDs = [
-  'addButton', 'substractButton', 'multiplicationButton', 'divisionButton'
-];
-const getCurrentNumber = () => inputNumber.value;
-const clearCurrentNumber = () => inputNumber.value = '';
+(
+  () => {
+    let operation = "";
+    const inputNumber = document.getElementById('inputNumber');
+    const operatorIDs = [
+      'addButton', 'substractButton', 'multiplicationButton', 'divisionButton'
+    ];
+    const getCurrentNumber = () => inputNumber.value;
+    const clearCurrentNumber = () => inputNumber.value = '';
 
-function clickOperators(event) {
-  const operator = event.target.innerText.trim();
-  const number = getCurrentNumber();
-  if(number == '') {
-    alert('You must type a number before')
-    return;
+    function clickOperators(event) {
+      const operator = event.target.innerText.trim();
+      const number = getCurrentNumber();
+      if(number == '') {
+        alert('You must type a number before')
+        return;
+      }
+      operation += `${number} ${operator} `;
+      clearCurrentNumber();
+    }
+
+    function clickPerformOperation() {
+      operation += getCurrentNumber();
+      const result = eval(operation)
+      document.getElementById('logInformation').value = `${operation} = ${result}`;
+      document.getElementById('resultValue').value = result;
+      clearCurrentNumber();
+      operation = '';
+    }
+
+    function clickClear() {
+      operation = '';
+      clearCurrentNumber();
+    }
+
+    operatorIDs.forEach((id) => (
+      document.getElementById(id).addEventListener('click', clickOperators))
+    );
+
+    document.getElementById('equalButton').
+      addEventListener('click', clickPerformOperation);
+
+    document.getElementById('resetButton').
+      addEventListener('click', clickClear);
   }
-  operation += `${number} ${operator} `;
-  clearCurrentNumber();
-}
-
-function clickPerformOperation() {
-  operation += getCurrentNumber();
-  document.getElementById('logInformation').value = operation;
-  document.getElementById('resultValue').value = eval(operation);
-  clearCurrentNumber();
-}
-
-function clickClear() {
-  operation = '';
-  clearCurrentNumber();
-}
-
-operatorIDs.forEach((id) => (
-  document.getElementById(id).addEventListener('click', clickOperators))
-);
-
-document.getElementById('equalButton').
-         addEventListener('click', clickPerformOperation);
-
-document.getElementById('resetButton').
-         addEventListener('click', clickClear);
+)();
